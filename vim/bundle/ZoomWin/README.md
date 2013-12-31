324aa22cf21335dae75cd6ef52deea423ed38b9f
Nadeem Bitar <nadeem@gmail.com>
2013-12-31 09:19:06 -0800

    Deleted vim bundles

--- vim/bundle/ZoomWin/README.md
+++
@@ -1,7 +0,0 @@
-ZoomWin
-=======
-
-Zoom in/out of windows (toggle between one window and multi-window)
-
-This is a mirror of (http://www.drchip.org/astronaut/vim/index.html#ZOOMWIN)
-
--- vim/bundle/gem-ctags/lib/rubygems_plugin.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems/command_manager'
-require 'rubygems/commands/ctags_command'
-
-Gem::CommandManager.instance.register_command :ctags
-
-Gem.post_install do |installer|
-  Gem::Commands::CtagsCommand.index(installer.spec)
-end
